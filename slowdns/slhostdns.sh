a8c9c44a5f895c532b2b7a5c7adcf7af8a952c1a
Bangs Ngek <70790541+BangsNgek@users.noreply.github.com>
2023-03-30 05:03:53 +0700

    Update slhostdns.sh

--- slowdns/slhostdns.sh
+++ slowdns/slhostdns.sh
@@ -9,11 +9,11 @@ rm nsdomain
 
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 subsl=$(</dev/urandom tr -dc a-x0-9 | head -c5)
-DOMAIN=mantapxsl.my.id
-SUB_DOMAIN=onichan-${sub}.mantapxsl.my.id
-NS_DOMAIN=slowdns-${subsl}.mantapxsl.my.id
-CF_ID=slinfinity69@gmail.com
-CF_KEY=dd2c5e0313f122b3c1833471d469b1025f492
+DOMAIN=bangsngeks.my.id
+SUB_DOMAIN=sg1.bangsngeks.my.id
+NS_DOMAIN=${subsl}.bangsngeks.my.id
+CF_ID=bangsngek@gmail.com
+CF_KEY=9b8c1ae3cbbe24bcf77b7d0c4b50d60ebb087
 echo "IP=""$SUB_DOMAIN" >> /var/lib/crot/subdomain.conf
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
